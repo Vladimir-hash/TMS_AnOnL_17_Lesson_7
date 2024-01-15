bbbacecbfde1bedd7665c0090ffc6a407451f23a
Vladimir-hash <borovikov_98@list.ru>
2024-01-15 18:37:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Phone phone = new Phone(111_11_11, "Samsung", 300);
+        Phone phone2 = new Phone(222_22_22, "Apple", 200);
+        Phone phone3 = new Phone(333_33_33, "Nokia", 350);
+        Phone phone4 = new Phone(444_44_44, "Asus");
+        Phone phone5 = new Phone();
+
+        System.out.println(phone);
+        System.out.println(phone2);
+        System.out.println(phone3);
+        System.out.println(phone4);
+        System.out.println(phone5);
+
+        phone.receiveCall("Tom");
+        phone2.receiveCall("Andrey");
+        phone3.receiveCall("Inna");
+        phone.getNumber();
+        phone2.getNumber();
+        phone3.getNumber();
+
+        phone.receiveCall("Andy", 777_77_77);
+        phone.sendMessage(111_11_11, 222_22_22, 333_33_33);
+    }
+}
---
+++ src/Phone.java
@@ -0,0 +1,67 @@
+/*
+Класс Phone.
+Создайте класс Phone, который содержит переменные number, model и
+weight.+
+Создайте три экземпляра этого класса.+
+Выведите на консоль значения их переменных. -
+Добавить в класс Phone методы: receiveCall, имеет один параметр – имя
+звонящего. Выводит на консоль сообщение “Звонит {name}”. getNumber –
+возвращает номер телефона. Вызвать эти методы для каждого из
+объектов.+
+Добавить конструктор в класс Phone, который принимает на вход три
+параметра для инициализации переменных класса - number, model и
+weight.+
+Добавить конструктор, который принимает на вход два параметра для
+инициализации переменных класса - number, model. +
+Добавить конструктор без параметров.+
+Вызвать из конструктора с тремя параметрами конструктор с двумя.
+Добавьте перегруженный метод receiveCall, который принимает два
+параметра - имя звонящего и номер телефона звонящего. Вызвать этот
+метод.
+Создать метод sendMessage с аргументами переменной длины. Данный
+метод принимает на вход номера телефонов, которым будет отправлено
+сообщение. Метод выводит на консоль номера этих телефонов.
+ */
+
+import java.util.Arrays;
+
+public class Phone {
+    public int number;
+    public String model;
+    public int weight;
+    public  Phone(int number, String model, int weight) {
+        this.model = model;
+        this.number = number;
+        this.weight = weight;
+    }
+    public  Phone(int number, String model) {
+        this.model = model;
+        this.number = number;
+    }
+    public  Phone() {
+
+    }
+    public  void receiveCall(String nameOfIncomingCall) {
+    System.out.println("Звонит: " + nameOfIncomingCall);
+}
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number = " + number +
+                ", model = '" + model + '\'' +
+                ", weight = " + weight +
+                '}';
+    }
+    public  void getNumber() {
+        System.out.println("Звонит: " + number);
+    }
+
+    public void receiveCall(String nameOfIncomingCall, int number) {
+        System.out.printf("Звонит %s\nНомер абонемента: %S\n", nameOfIncomingCall, number);
+    }
+    public void sendMessage(int... numbers) {
+        System.out.println("Сообщение отправлено на номера:");
+        System.out.println(Arrays.toString(numbers));
+    }
+}
